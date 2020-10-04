a242349772d14955b8e60d7bedb54a79487f6a8f
MON@H <monah.rasta@gmail.com>
2020-10-04 15:30:58 +0300

    Extended version by MON@H

--- LootMultiplier.cs
+++ LootMultiplier.cs
@@ -1,128 +1,117 @@
-using System.Collections.Generic;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Oxide.Plugins
 {
-    [Info("Loot Multiplier", "Orange", "1.1.2")]
+    [Info("Loot Multiplier", "Orange", "1.2.0")]
     [Description("Multiply items in all loot containers in the game")]
     public class LootMultiplier : RustPlugin
     {
-        #region Oxide Hooks
-        
-        private void OnLootSpawn(StorageContainer container)
-        {
-            timer.Once(config.delay, () =>
-            {
-                Multiply(container);
-            });
-        }
-        
-        #endregion
-
-        #region Core
-
-        private void Multiply(StorageContainer container)
-        {
-            if (container == null)
-            {
-                return;
-            }
-
-            var multiplier = 0;
-            if (config.containers.TryGetValue(container.ShortPrefabName, out multiplier) == false)
-            {
-                return;
-            }
-
-            foreach (var item in container.inventory.itemList.ToArray())
-            {
-                var shortname = item.info.shortname;
-                var category = item.info.category.ToString();
-                
-                if (config.blacklist.Contains(shortname) || config.blacklist.Contains(category))
-                {
-                    continue;
-                }
-                
-                if (item.hasCondition && config.multiplyItemsWithCondition == false)
-                {
-                    continue;
-                }
-                
-                item.amount *= multiplier;
-            }
-        }
-
-        #endregion
-        
         #region Configuration
         
-        private static ConfigData config;
+        private static ConfigData configData;
         
         private class ConfigData
         {
-            [JsonProperty(PropertyName = "Shortname -> Multiplier")]
-            public Dictionary<string, int> containers = new Dictionary<string, int>();
+            [JsonProperty(PropertyName = "Global settings")]
+            public GlobalSettings globalS = new GlobalSettings();
 
-            [JsonProperty(PropertyName = "Multiply items with condition")]
-            public bool multiplyItemsWithCondition;
+            [JsonProperty(PropertyName = "Item settings")]
+            public ItemSettings itemS = new ItemSettings();
 
-            [JsonProperty(PropertyName = "Delay after spawning crate to multiply it")]
-            public float delay;
-
-            [JsonProperty(PropertyName = "Item Blacklist")]
-            public List<string> blacklist = new List<string>();
-        }
-        
-        private ConfigData GetDefaultConfig()
-        {
-            return new ConfigData 
+            public class GlobalSettings
             {
-                multiplyItemsWithCondition = false,
-                delay = 1f,
-                containers = new Dictionary<string, int>
+                [JsonProperty(PropertyName = "Default Multiplier for new containers")]
+                public int defaultContainerMultiplier = 1;
+
+                [JsonProperty(PropertyName = "Default Multiplier for new Categories")]
+                public int defaultCategoryMultiplier = 1;
+
+                [JsonProperty(PropertyName = "Multiply items with condition")]
+                public bool multiplyItemsWithCondition = false;
+
+                [JsonProperty(PropertyName = "Delay after spawning container to multiply it")]
+                public float delay = 1f;
+            }
+
+            public class ItemSettings
+            {
+                [JsonProperty(PropertyName = "Containers list (shortPrefabName: multiplier)")]
+                public Dictionary<string, int> containers = new Dictionary<string, int>()
                 {
-                    {"loot-barrel-1", 2},
-                    {"loot-barrel-2", 2},
-                    {"loot_barrel_1", 2},
-                    {"loot_barrel_2", 2},
-                    {"crate_underwater_basic", 2},
-                    {"crate_underwater_advanced", 2},
-                    {"foodbox", 2},
-                    {"trash-pile-1", 2},
-                    {"minecart", 2},
-                    {"oil_barrel", 2},
-                    {"crate_basic", 2},
-                    {"crate_mine", 2},
-                    {"crate_tools", 2},
-                    {"crate_normal", 2},
-                    {"crate_normal_2", 2},
-                    {"crate_normal_2_food", 2},
-                    {"crate_normal_2_medical", 2},
-                    {"crate_elite", 2},
-                    {"codelockedhackablecrate", 2},
-                    {"bradley_crate", 2},
-                    {"heli_crate", 2},
-                    {"supply_drop", 2}
-                },
-                blacklist = new List<string>
+                    {"bradley_crate", 1},
+                    {"codelockedhackablecrate", 1},
+                    {"codelockedhackablecrate_oilrig", 1},
+                    {"crate_basic", 1},
+                    {"crate_elite", 1},
+                    {"crate_mine", 1},
+                    {"crate_normal", 1},
+                    {"crate_normal_2", 1},
+                    {"crate_normal_2_food", 1},
+                    {"crate_normal_2_medical", 1},
+                    {"crate_tools", 1},
+                    {"crate_underwater_advanced", 1},
+                    {"crate_underwater_basic", 1},
+                    {"foodbox", 1},
+                    {"heli_crate", 1},
+                    {"loot-barrel-1", 1},
+                    {"loot-barrel-2", 1},
+                    {"loot_barrel_1", 1},
+                    {"loot_barrel_2", 1},
+                    {"minecart", 1},
+                    {"oil_barrel", 1},
+                    {"supply_drop", 1},
+                    {"trash-pile-1", 1},
+                    {"vehicle_parts", 1}
+                };
+
+                [JsonProperty(PropertyName = "Categories list (Category: multiplier)")]
+                public Dictionary<string, int> categories = new Dictionary<string, int>()
                 {
-                    "cctv.camera",
-                    "targeting.computer",
-                    "Weapon"
-                }
-            };
+                    {"Ammunition", 1},
+                    {"Attire", 1},
+                    {"Component", 1},
+                    {"Construction", 1},
+                    {"Electrical", 1},
+                    {"Food", 1},
+                    {"Fun", 1},
+                    {"Items", 1},
+                    {"Medical", 1},
+                    {"Misc", 1},
+                    {"Resources", 1},
+                    {"Tool", 1},
+                    {"Traps", 1},
+                    {"Weapon", 1}
+                };
+
+                [JsonProperty(PropertyName = "Items list (shortname: multiplier)")]
+                public Dictionary<string, int> items = new Dictionary<string, int>()
+                {
+                    {"metalpipe", 1},
+                    {"scrap", 1},
+                    {"tarp", 1}
+                };
+
+                [JsonProperty(PropertyName = "Item | Category Blacklist", ObjectCreationHandling = ObjectCreationHandling.Replace)]
+                public List<string> blacklist = new List<string>()
+                {
+                    {"ammo.rocket.smoke"},
+                    {"Attire"},
+                    {"Weapon"}
+                };
+            }
         }
-        
+
         protected override void LoadConfig()
         {
             base.LoadConfig();
-   
+
             try
             {
-                config = Config.ReadObject<ConfigData>();
-        
-                if (config == null)
+                configData = Config.ReadObject<ConfigData>();
+
+                if (configData == null)
                 {
                     LoadDefaultConfig();
                 }
@@ -137,15 +126,89 @@ namespace Oxide.Plugins
 
         protected override void LoadDefaultConfig()
         {
-            PrintError("Configuration file is corrupt(or not exists), creating new one!");
-            config = GetDefaultConfig();
+            PrintWarning("Creating a new configuration file");
+            configData = new ConfigData();
         }
 
         protected override void SaveConfig()
         {
-            Config.WriteObject(config);
+            Config.WriteObject(configData);
+        }
+
+        #endregion
+
+        #region Oxide Hooks
+
+        private void OnLootSpawn(StorageContainer container)
+        {
+            timer.Once(configData.globalS.delay, () =>
+            {
+                Multiply(container);
+            });
         }
         
+        #endregion
+
+        #region Core
+
+        private void Multiply(StorageContainer container)
+        {
+            if (container == null) return;
+
+            var multiplier = 0;
+            var containerName = container.ShortPrefabName;
+            if (!configData.itemS.containers.ContainsKey(containerName))
+            {
+                configData.itemS.containers.Add(containerName, configData.globalS.defaultContainerMultiplier);
+                Dictionary<string, int> sorted = new Dictionary<string, int>();
+                foreach (KeyValuePair<string, int> cont in configData.itemS.containers.OrderBy(key => key.Key))
+                {
+                    sorted.Add(cont.Key, cont.Value);
+                }
+                configData.itemS.containers = sorted;
+                SaveConfig();
+            }
+
+            multiplier = configData.itemS.containers[containerName];
+
+            foreach (var item in container.inventory.itemList.ToArray())
+            {
+                var shortname = item.info.shortname;
+                var category = item.info.category.ToString();
+
+                if (!configData.itemS.categories.ContainsKey(category))
+                {
+                    configData.itemS.categories.Add(category, configData.globalS.defaultCategoryMultiplier);;
+                    Dictionary<string, int> sorted = new Dictionary<string, int>();
+                    foreach (KeyValuePair<string, int> cont in configData.itemS.categories.OrderBy(key => key.Key))
+                    {
+                        sorted.Add(cont.Key, cont.Value);
+                    }
+                    configData.itemS.categories = sorted;
+                    SaveConfig();
+                }
+
+                if (configData.itemS.blacklist.Contains(shortname) || configData.itemS.blacklist.Contains(category))
+                {
+                    continue;
+                }
+                
+                if (item.hasCondition && configData.globalS.multiplyItemsWithCondition == false)
+                {
+                    continue;
+                }
+
+                var itemMultiplier = configData.itemS.categories[category];
+                if (configData.itemS.items.ContainsKey(shortname))
+                {
+                    itemMultiplier = itemMultiplier * configData.itemS.items[shortname];
+                }
+
+                multiplier = multiplier * itemMultiplier;
+                item.amount *= (multiplier > 1 ? multiplier : 1);
+            }
+        }
+
         #endregion
     }
 }
--- README.md
+++ README.md
@@ -1,33 +1,95 @@
-`Loot Multiplier` can multiply any loot from any container in the game. Compatible with all loot plugins
+`Loot Multiplier` can multiply any loot from any container in the game. Compatible with all loot plugins.
+
+All multipliers set to 1 by default and has no impact, you can install plugin safely and then edit default config file to your needs.
+There are 4 types of lists:
+*`Containers list` - is the list of all containers. All new containers automaticly added into this list.
+*`Categories list` - is the list of all categories. All new categories automaticly added into this list.
+*`Items list` - is the list of items. You can edit this list as you want.
+*`Item|Category Blacklist` - is the list of items that will be skipped by this plugin. You can edit this list as you want.
+
+There are 3 multiplier types: Container, Category, Item. Consider that all them 3 will apply to item.
+Blacklist overrides all other settings, so if you add item or category to blacklist this plugin will ignore it.
+
+
+*`Example 1`: you want multiply only scrap in all containers x2.
+You need to add `"scrap": 2` to your items list and you're done.
+
+*`Example 2`: you want to multiply all loot in underwater crates x2 and heli crates x4.
+You need to set `"crate_underwater_advanced": 2`, `"crate_underwater_basic": 2` and `"heli_crate": 4`.
+
+*`Example 3`: you want to multiply all metal pipes x2, all elite crates loot x2, and all components x4.
+You can't do exactly this one. First you set `"crate_elite": 2`, then `"Component": 2`.
+This way for all Components in all Elite Crates you will get x2 (Elite Crate multiplier) * x2 (Component multiplier) = x4.
+All components in other containers will have x2 multiplier.
+Now, if you set `"metalpipe": 4` - you will have x16 multiplier for Metal Pipe in Elite Crates (x2 * x2 * x4).
+You will have x8 multiplier for Metal Pipe in other containers (x4 * x2).
+Also, if you will add, let's say Tech Trash to blacklist, it will have x1 multiplier no matter of all other settings.
+
 
 ## Configuration
 
 ```json
 {
-  "Shortname -> Multiplier": {
-    "loot-barrel-1": 2,
-    "loot-barrel-2": 2,
-    "loot_barrel_1": 2,
-    "loot_barrel_2": 2,
-    "crate_basic": 2,
-    "crate_mine": 2,
-    "crate_tools": 2,
-    "crate_normal": 2,
-    "crate_normal_2": 2,
-    "crate_normal_2_food": 2,
-    "crate_normal_2_medical": 2,
-    "crate_elite": 2,
-    "codelockedhackablecrate": 2,
-    "bradley_crate": 2,
-    "heli_crate": 2,
-    "supply_drop": 2
+  "Global settings": {
+    "Default Multiplier for new containers": 1,
+    "Default Multiplier for new Categories": 1,
+    "Multiply items with condition": false,
+    "Delay after spawning container to multiply it": 1.0
   },
-  "Multiply items with condition": false,
-  "Delay after spawning crate to multiply it": 1.0,
-  "Item Blacklist": [
-    "cctv.camera",
-    "targeting.computer"
-  ]
+  "Item settings": {
+    "Containers list (shortPrefabName: multiplier)": {
+      "bradley_crate": 1,
+      "codelockedhackablecrate": 1,
+      "codelockedhackablecrate_oilrig": 1,
+      "crate_basic": 1,
+      "crate_elite": 1,
+      "crate_mine": 1,
+      "crate_normal": 1,
+      "crate_normal_2": 1,
+      "crate_normal_2_food": 1,
+      "crate_normal_2_medical": 1,
+      "crate_tools": 1,
+      "crate_underwater_advanced": 1,
+      "crate_underwater_basic": 1,
+      "foodbox": 1,
+      "heli_crate": 1,
+      "loot-barrel-1": 1,
+      "loot-barrel-2": 1,
+      "loot_barrel_1": 1,
+      "loot_barrel_2": 1,
+      "minecart": 1,
+      "oil_barrel": 1,
+      "supply_drop": 1,
+      "trash-pile-1": 1,
+      "vehicle_parts": 1
+    },
+    "Categories list (Category: multiplier)": {
+      "Ammunition": 1,
+      "Attire": 1,
+      "Component": 1,
+      "Construction": 1,
+      "Electrical": 1,
+      "Food": 1,
+      "Fun": 1,
+      "Items": 1,
+      "Medical": 1,
+      "Misc": 1,
+      "Resources": 1,
+      "Tool": 1,
+      "Traps": 1,
+      "Weapon": 1
+    },
+    "Items list (shortname: multiplier)": {
+      "metalpipe": 1,
+      "scrap": 1,
+      "tarp": 1
+    },
+    "Item | Category Blacklist": [
+      "ammo.rocket.smoke",
+      "Attire",
+      "Weapon"
+    ]
+  }
 }
 ```
 
